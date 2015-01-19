9505fc01e44f8808f45406bbad438a549cef283c
rjurado01 <rjurado@openmailbox.org>
2015-01-19 15:21:14 +0100

    Refactoring + Widget API.

--- javascripts/controllers/home_controller.js
+++ javascripts/controllers/home_controller.js
@@ -1,45 +1,99 @@
 Soud.controller('homeController', ['$scope', '$sce', function($scope, $sce) {
-  $scope.activities_all = [];
-  $scope.activities_uploaded = [];
-  $scope.activities = $scope.activities_all;
+  $scope.activities = [];
+  $scope.widgets = [];
   $scope.limit = 10;
   $scope.next_href;
+  $scope.filter = 'uploaded';
+  $scope.play = false;
 
   $scope.showUploaded = function() {
     $scope.activities = $scope.activities_uploaded;
+    $scope.filter = 'uploaded';
+
+    // reset all
+    $scope.activities = [];
+    $scope.widgets = [];
+
+    // get first page
+    getPage('/me/activities');
   };
 
   $scope.showAll = function() {
-    $scope.activities = $scope.activities_all;
+    $scope.activities = $scope.activities;
+    $scope.filter = 'all';
+
+    // reset all
+    $scope.filter = 'all';
+    $scope.activities = [];
+
+    // get first page
+    getPage('/me/activities');
   };
 
   $scope.nextPage = function() {
-    $scope.getPage($scope.next_href);
+    getPage($scope.next_href);
   };
 
-  $scope.getPage = function(url) {
+  function getPage(url) {
     SC.get(url, { limit: $scope.limit, linked_partitioning: 1 }, function(activities) {
       $scope.next_href = activities.next_href;
+      addActivities(activities.collection);
+    });
+  };
 
-      activities.collection.forEach(function(activity) {
-        var options = {auto_play: false, maxheight: 166, iframe: true, color: 'ff5500'};
+  function addActivities(activities) {
+    var activity = activities.shift();
 
-        if( activity.type.indexOf('playlist') > -1 )
-          options.maxheight = 300;
+    if( $scope.filter == 'all' || activity.type.indexOf('repost') == -1 ) {
+      var options = {auto_play: false, maxheight: 166, iframe: true, color: 'ff5500'};
 
-        SC.oEmbed(activity.origin.uri, options, function(oEmbed) {
-          activity.html = $sce.trustAsHtml(oEmbed.html.replace('visual=true&',''));
-          $scope.activities_all.push(activity);
-          $scope.$apply();
+      if( activity.type.indexOf('playlist') > -1 )
+        options.maxheight = 300;
 
-          if(activity.type.indexOf('repost') == -1) {
-            $scope.activities_uploaded.push(activity);
-          }
-        });
+      // get embeded code
+      SC.oEmbed(activity.origin.uri, options, function(oEmbed) {
+        // add html to page
+        activity.html = $sce.trustAsHtml(oEmbed.html.replace('visual=true&',''));
+        $scope.activities.push(activity);
+        $scope.$apply();
 
-        $scope.offset += $scope.limit;
+        // create widget
+        var iframe = document.getElementById(activity.origin.id).querySelector('iframe');
+        var widget = SC.Widget(iframe);
+        var previous_width = $scope.widgets[$scope.widgets.length-1];
+
+        if( previous_width ) {
+          previous_width.bind(SC.Widget.Events.FINISH, function(){
+            widget.play();
+          });
+        }
+
+        if( $scope.play ) {
+          $scope.play = false;
+
+          setTimeout(function() {
+            widget.play();
+          }, 1000);
+        }
+
+        activity.widget = widget;
+        $scope.widgets.push(widget);
+
+        // proccess next activity
+        if( activities.length > 0 ) {
+          addActivities(activities);
+        }
+        else {
+          widget.bind(SC.Widget.Events.FINISH, function(){
+            $scope.nextPage();
+            $scope.play = true;
+          });
+        }
       });
-    });   
+    }
+    else if( activities.length > 0 ) {
+      addActivities(activities);
+    };
   };
 
   initialize();
@@ -54,8 +108,8 @@ Soud.controller('homeController', ['$scope', '$sce', function($scope, $sce) {
         redirect_uri: 'http://localhost/soud/callback.html',
         access_token: soud_token,
       });
-   
-      $scope.getPage('/me/activities');
+
+      getPage('/me/activities');
     }
     else {
       // initialize client with app credentials
@@ -67,7 +121,7 @@ Soud.controller('homeController', ['$scope', '$sce', function($scope, $sce) {
       // initiate auth popup
       SC.connect(function() {
         SC.get('/me', function(me) {
-          var sound_token = console.log(localStorage.getItem('soud_token'));
+          var sound_token = localStorage.getItem('soud_token');
           var user = me.username;
         });
       });
