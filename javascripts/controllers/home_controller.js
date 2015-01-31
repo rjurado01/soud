89c4ba0c92afeb046a9ab76d13acaac775e1cab4
rjurado01 <rjurado@openmailbox.org>
2015-01-31 23:17:16 +0100

    Added filters support. Remove recomended tracks when track ends.

--- javascripts/controllers/home_controller.js
+++ javascripts/controllers/home_controller.js
@@ -4,101 +4,124 @@ Soud.controller('homeController', ['$scope', '$sce', function($scope, $sce) {
   $scope.limit = 10;
   $scope.next_href;
   $scope.filter = 'uploaded';
-  $scope.play = false;
 
-  $scope.showUploaded = function() {
-    $scope.activities = $scope.activities_uploaded;
-    $scope.filter = 'uploaded';
+  /*
+   * Reset activities list and show new selected activities
+   */
+  $scope.showActivities = function(filter) {
+    if( filter != $scope.filter ) {
+      $scope.filter = filter;
 
-    // reset all
-    $scope.activities = [];
-    $scope.widgets = [];
+      // reset all
+      $scope.activities = [];
+      $scope.widgets = [];
 
-    // get first page
-    getPage('/me/activities');
+      // get first page
+      getPage('/me/activities');
+    }
   };
 
-  $scope.showAll = function() {
-    $scope.activities = $scope.activities;
-    $scope.filter = 'all';
-
-    // reset all
-    $scope.filter = 'all';
-    $scope.activities = [];
-
-    // get first page
-    getPage('/me/activities');
-  };
-
+  /*
+   * Get next page of activities
+   */
   $scope.nextPage = function() {
     getPage($scope.next_href);
   };
 
+  /*
+   * Request the API page for this url and add activities to list
+   */
   function getPage(url) {
     SC.get(url, { limit: $scope.limit, linked_partitioning: 1 }, function(activities) {
       $scope.next_href = activities.next_href;
-      addActivities(activities.collection);
+      addActivities( filterActivities(activities.collection) );
     });
   };
 
+  /*
+   * Remove first activity from array, add it to page and create widget for it.
+   * If array isn't empty, call this function with it again
+   */
   function addActivities(activities) {
     var activity = activities.shift();
+    var options = {auto_play: false, maxheight: 166, iframe: true, color: 'ff5500'};
 
-    if( $scope.filter == 'all' || activity.type.indexOf('repost') == -1 ) {
-      var options = {auto_play: false, maxheight: 166, iframe: true, color: 'ff5500'};
+    if( activity.type.indexOf('playlist') > -1 )
+      options.maxheight = 300;
 
-      if( activity.type.indexOf('playlist') > -1 )
-        options.maxheight = 300;
+    // get embeded code
+    SC.oEmbed(activity.origin.uri, options, function(oEmbed) {
+      // add html to page
+      activity.html = $sce.trustAsHtml(oEmbed.html.replace('visual=true&',''));
+      $scope.activities.push(activity);
+      $scope.$apply();
 
-      // get embeded code
-      SC.oEmbed(activity.origin.uri, options, function(oEmbed) {
-        // add html to page
-        activity.html = $sce.trustAsHtml(oEmbed.html.replace('visual=true&',''));
-        $scope.activities.push(activity);
-        $scope.$apply();
+      // create widget
+      var iframe = document.getElementById(activity.origin.id).querySelector('iframe');
+      activity.widget = SC.Widget(iframe);
 
-        // create widget
-        var iframe = document.getElementById(activity.origin.id).querySelector('iframe');
-        var widget = SC.Widget(iframe);
-        var previous_width = $scope.widgets[$scope.widgets.length-1];
+      // add auto play when previous track end
+      addNextAutoPlay(activity);
 
-        if( previous_width ) {
-          previous_width.bind(SC.Widget.Events.FINISH, function(){
-            widget.play();
-          });
-        }
-
-        if( $scope.play ) {
-          $scope.play = false;
-
-          setTimeout(function() {
-            widget.play();
-          }, 1000);
-        }
-
-        activity.widget = widget;
-        $scope.widgets.push(widget);
-
+      if( activities.length > 0 ) {
         // proccess next activity
-        if( activities.length > 0 ) {
-          addActivities(activities);
-        }
-        else {
-          widget.bind(SC.Widget.Events.FINISH, function(){
-            $scope.nextPage();
-            $scope.play = true;
-          });
-        }
-      });
-    }
-    else if( activities.length > 0 ) {
-      addActivities(activities);
+        addActivities(activities);
+      }
+      else {
+        // get next page
+        activity.widget.bind(SC.Widget.Events.PLAY, function(){
+          $scope.nextPage();
+        });
+      }
+    });
+  };
+
+  /*
+   * Filter and return activities
+   */
+  function filterActivities(activities) {
+    switch($scope.filter) {
+      case 'uploaded':
+        return activities.filter(function(activity){
+          return activity.type.indexOf('repost') == -1 && activity.type.indexOf('playlist') == -1
+        });
+      case 'repost':
+        return activities.filter(function(activity){
+          return activity.type.indexOf('repost') > -1 && activity.type.indexOf('playlist') == -1
+        });
     };
+
+    return activities;
+  };
+
+  /*
+   * Add event to previout activity when it ends for play next track
+   */
+  function addNextAutoPlay(activity) {
+    var index = $scope.activities.indexOf(activity);
+
+    // Check if this is the first activity
+    if( index > 0 ) {
+      var previous_activity = $scope.activities[index - 1];
+
+      if( previous_activity ) {
+        previous_activity.widget.bind(SC.Widget.Events.FINISH, function(){
+          setTimeout(function(){
+            // reload previous activity to remove "recommended tracks"
+            previous_activity.widget.load(previous_activity.origin.uri);
+
+            // play previous activity
+            activity.widget.play();
+          }, 500);
+        });
+      }
+    }
   };
 
   initialize();
 
   function initialize() {
+    // get soud_token (remember previous login)
     soud_token = localStorage.getItem('soud_token');
 
     if( soud_token ) {
@@ -120,10 +143,7 @@ Soud.controller('homeController', ['$scope', '$sce', function($scope, $sce) {
 
       // initiate auth popup
       SC.connect(function() {
-        SC.get('/me', function(me) {
-          var sound_token = localStorage.getItem('soud_token');
-          var user = me.username;
-        });
+        getPage('/me/activities');
       });
     };
   };
